9fdc0424813c3130d6a1b37589c2d92156c873cf
ksandeleon <ksanureeves1@gmail.com>
2025-04-21 21:02:25 +0800

    add initial file structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:true_studentmgnt_mobapp/frontend/dashboard_screen.dart';
+
+import 'package:true_studentmgnt_mobapp/frontend/login_screen.dart';
+import 'package:true_studentmgnt_mobapp/frontend/signup_screen.dart';
+import 'package:true_studentmgnt_mobapp/frontend/welcome_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
@@ -10,59 +17,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GO STUDENT',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      initialRoute: 'welcome_screen',
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(), // id = "welcome_screen"
+        LoginScreen.id: (context) => LoginScreen(), //id = "login_screen"
+        SignupScreen.id: (context) => SignupScreen(), //id = "signup_screen"
+        DashboardScreen.id: (context) => DashboardScreen(), // id = "dashboard_screen"
+        
+      },
     );
   }
 }
