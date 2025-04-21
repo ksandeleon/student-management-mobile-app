749a78c7535fbcaaf369e18649c07d422c21b7bf
ksandeleon <ksanureeves1@gmail.com>
2025-04-21 21:17:41 +0800

    add constants on utilities

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+// App Colors
+const Color kPrimaryColor = Color(0xFF210F37);
+const Color kSecondaryColor = Color(0xFF4F1C51);
+const Color kAccentColor = Color(0xFFA55B4B);
+const Color kComplementaryColor = Color(0xFFDCA06D);
+
+// Text Styles
+const TextStyle kHeadingTextStyle = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+  // Color will be taken from theme
+);
+
+const TextStyle kSubheadingTextStyle = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  // Color will be taken from theme
+);
+
+const TextStyle kBodyTextStyle = TextStyle(
+  fontSize: 16,
+  // Color will be taken from theme
+);
+
+// Spacing
+const double kDefaultPadding = 16.0;
+const double kSmallPadding = 8.0;
+const double kLargePadding = 24.0;
+
+// Border Radius
+const double kDefaultBorderRadius = 8.0;
+const double kLargeBorderRadius = 16.0;
